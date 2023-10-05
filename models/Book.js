9a4f73440b4d676178b51a12322bdcdb625abfb7
Yash Gupta <ygupta2001@gmail.com>
2023-10-05 11:47:29 +0530

    Modified according to the requirements

--- models/Book.js
+++ models/Book.js
@@ -1,18 +1,48 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
+    bookID: {
+        type: Number,
+        required: true,
+        unique: true
+    },
     title: {
         type: String,
         required: true
     },
-    author: {
+    authors: {
         type: String,
         required: true
+    },
+    average_rating: {
+        type: String
     },
     isbn: {
         type: String,
         unique: true
     },
+    isbn13: {
+        type: String,
+        unique: true
+    },
+    language_code: {
+        type: String
+    },
+    num_pages: {
+        type: String
+    },
+    ratings_count: {
+        type: Number
+    },
+    text_reviews_count: {
+        type: Number
+    },
+    publication_date: {
+        type: String
+    },
+    publisher: {
+        type: String
+    }
 });
 
 const Book = mongoose.model('Book', bookSchema);
--- models/User.js
+++ models/User.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {  // hashed before storing
+    name: {
         type: String,
         required: true
     },
@@ -15,8 +11,33 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    // we can add any field like, profilePic, address, etc.
+    password: {
+        type: String,
+        required: true
+    },
+    wishlist: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book'
+    }],
+    purchasedBooks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book'
+    }]
 });
+
+// hashing the password before saving a new user
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// checking if an entered password is correct
+userSchema.methods.correctPassword = async function(enteredPassword, userPassword) {
+    return await bcrypt.compare(enteredPassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
