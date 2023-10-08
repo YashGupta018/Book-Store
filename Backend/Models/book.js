c74dd9e6db1c5ac1f6330e216bf4fc9b566066eb
Yash Gupta <ygupta2001@gmail.com>
2023-10-09 02:34:56 +0530

    price datatype changed to string

--- Backend/Models/book.js
+++ Backend/Models/book.js
@@ -15,7 +15,7 @@ const bookSchema = new mongoose.Schema({
         required: true
     },
     price: {
-        type: Number,
+        type: String,
         require: true
     },
     description: {
